5d9a132c0778a9d47c7d95024e680721263e0177
Ebooth <pauldejeandev@gmail.com>
2023-10-10 10:37:48 +0200

    Setup of project

---
+++ .eslintrc.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  root: true,
+  extends: ["standard-with-typescript"],
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "off",
+  },
+};
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+function main() {
+  console.log("setup done");
+}
+
+main();
